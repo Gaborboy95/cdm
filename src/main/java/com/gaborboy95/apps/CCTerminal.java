eefac29cb6d2bfdb3fe0dabbe7a5ad9de492998a
Peter Gabor <peter.gabor04@gmail.com>
2017-12-26 12:37:00 +0100

    Initial commit

---
+++ src/main/java/com/gaborboy95/apps/CCTerminal.java
@@ -0,0 +1,20 @@
+package com.gaborboy95.apps;
+
+//The author of this mod is Gaborboy95
+
+import com.gaborboy95.apps.app.TerminalApp;
+import com.mrcrayfish.device.api.ApplicationManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MC_VERSION, dependencies = Reference.DEPENDS)
+public class CCTerminal
+{
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        ApplicationManager.registerApplication(new ResourceLocation(Reference.MOD_ID,"terminalapp"), TerminalApp.class);
+    }
+
+}
