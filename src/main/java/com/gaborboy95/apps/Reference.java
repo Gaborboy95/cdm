d100692b8e3c3ef507e2340fa524b7c655cc9ac0
Peter Gabor <peter.gabor04@gmail.com>
2017-12-26 12:43:05 +0100

    Revert "Initial commit"
    This reverts commit eefac29cb6d2bfdb3fe0dabbe7a5ad9de492998a.

--- src/main/java/com/gaborboy95/apps/Reference.java
+++
@@ -1,9 +0,0 @@
-package com.gaborboy95.apps;
-
-public class Reference {
-    public static final String MOD_ID = "app_computercraft_terminal";
-    public static final String MOD_NAME = "Computercraft terminal for cdm";
-    public static final String MOD_VERSION = "0.0.1";
-    public static final String MC_VERSION = "[1.12.2]";
-    public static final String DEPENDS = "required-after:cdm@[0.0.2-pre5,)";
-}
