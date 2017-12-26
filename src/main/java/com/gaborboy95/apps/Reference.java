0deba84104a860506ad9a8e73bfad08f70ed7b6a
Gaborboy95 <peter.gabor04@gmail.com>
2017-12-26 20:31:40 +0100

    fixed depends arg

--- src/main/java/com/gaborboy95/apps/Reference.java
+++ src/main/java/com/gaborboy95/apps/Reference.java
@@ -5,5 +5,5 @@ public class Reference {
     public static final String MOD_NAME = "Computercraft terminal for cdm";
     public static final String MOD_VERSION = "0.0.1";
     public static final String MC_VERSION = "[1.12.2]";
-    public static final String DEPENDS = "required-after:cdm@[0.0.2-pre5,)";
+    public static final String DEPENDS = "required-after:cdm@[0.2.0-pre6,)";
 }
