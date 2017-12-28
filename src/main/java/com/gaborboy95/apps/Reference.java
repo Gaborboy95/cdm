93a03ecc182bb110124a1cf58b31004c1e1bbc10
Peter Gabor <peter.gabor04@gmail.com>
2017-12-28 11:14:22 +0100

    Renamed modid and added remodelled laptop

--- src/main/java/com/gaborboy95/apps/Reference.java
+++ src/main/java/com/gaborboy95/apps/Reference.java
@@ -1,7 +1,7 @@
 package com.gaborboy95.apps;
 
 public class Reference {
-    public static final String MOD_ID = "app_computercraft_terminal";
+    public static final String MOD_ID = "cdm_cctapp";
     public static final String MOD_NAME = "Computercraft terminal for cdm";
     public static final String MOD_VERSION = "0.0.1";
     public static final String MC_VERSION = "[1.12.2]";
