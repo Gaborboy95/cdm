694eec403879a80f99b8c6d77b8291641118e8eb
Peter Gabor <peter.gabor04@gmail.com>
2017-12-28 08:05:43 +0100

    Removed label and textarea and made the program "fullscreen"

--- src/main/java/com/gaborboy95/apps/app/TerminalApp.java
+++ src/main/java/com/gaborboy95/apps/app/TerminalApp.java
@@ -1,21 +1,16 @@
 package com.gaborboy95.apps.app;
 
 import com.mrcrayfish.device.api.app.Application;
-import com.mrcrayfish.device.api.app.component.Label;
-import com.mrcrayfish.device.api.app.component.TextArea;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class TerminalApp extends Application
 {
-    private Label WIP;
-    private TextArea textWIP;
 
     @Override
     public void init() {
-        WIP = new Label("CC Lua terminal app WIP!", 5,5);
-        this.addComponent(WIP);
-        textWIP = new TextArea(5,15,190,80);
-        this.addComponent(textWIP);
+        this.setDefaultWidth(500);
+        this.setDefaultHeight(300);
+        
     }
 
     @Override
