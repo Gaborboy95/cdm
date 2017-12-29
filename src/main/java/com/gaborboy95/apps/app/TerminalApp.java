85f00a1aeb8c1085c7113d3890f2b2b1671f541a
Peter Gabor <peter.gabor04@gmail.com>
2017-12-29 09:15:11 +0100

    added luaj 3.0.1

--- src/main/java/com/gaborboy95/apps/app/TerminalApp.java
+++ src/main/java/com/gaborboy95/apps/app/TerminalApp.java
@@ -1,18 +1,21 @@
 package com.gaborboy95.apps.app;
 
 import com.mrcrayfish.device.api.app.Application;
+import com.mrcrayfish.device.api.app.component.TextField;
 import net.minecraft.nbt.NBTTagCompound;
+import org.luaj.vm2.*;
+import org.luaj.vm2.lib.*;
 
 public class TerminalApp extends Application
 {
-
+    public TextField textfield;
 
     @Override
     public void init() {
         this.setDefaultWidth(500);
-        this.setDefaultHeight(300);
-
-
+        this.setDefaultHeight(250);
+        textfield = new TextField(0,200,200);
+        this.addComponent(textfield);
     }
 
     @Override
