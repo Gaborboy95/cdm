b315119b85c0fd6eadd8cedf3bc1dfbdc88e2efc
Peter Gabor <peter.gabor04@gmail.com>
2017-12-29 08:11:28 +0100

    'chopped' down the modid a bit, and really fixed icon for this time.

--- src/main/java/com/gaborboy95/apps/Reference.java
+++ src/main/java/com/gaborboy95/apps/Reference.java
@@ -1,7 +1,7 @@
 package com.gaborboy95.apps;
 
 public class Reference {
-    public static final String MOD_ID = "cdm_cctapp";
+    public static final String MOD_ID = "cctapp";
     public static final String MOD_NAME = "Computercraft terminal for cdm";
     public static final String MOD_VERSION = "0.1.8";
     public static final String MC_VERSION = "[1.12.2]";
